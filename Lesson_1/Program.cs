2516ceb1d5ddc13badb9d60a31be8b11851e1144
iachichurov <a.iachichurov@gmail.com>
2022-09-18 03:31:01 +1000

    test added

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -38,50 +38,68 @@
 
 // Console.Write(max);
 
-Console.Clear();
+// Console.Clear();
 
-int xa = 50;
-int ya = 1;
-int xb = 1;
-int yb = 20;
-int xc = 100;
-int yc = 20;
+// int xa = 50;
+// int ya = 1;
+// int xb = 1;
+// int yb = 20;
+// int xc = 100;
+// int yc = 20;
 
-Console.SetCursorPosition(xa, ya);
-Console.WriteLine("+");
+// Console.SetCursorPosition(xa, ya);
+// Console.WriteLine("+");
 
-Console.SetCursorPosition(xb, yb);
-Console.WriteLine("+");
+// Console.SetCursorPosition(xb, yb);
+// Console.WriteLine("+");
 
-Console.SetCursorPosition(xc, yc);
-Console.WriteLine("+");
+// Console.SetCursorPosition(xc, yc);
+// Console.WriteLine("+");
 
-int x = xa;
-int y = ya;
+// int x = xa;
+// int y = ya;
 
-int count = 0;
+// int count = 0;
 
-while(count < 100000)
+// while(count < 100000)
+// {
+//     int randot = new Random().Next(0, 3);
+//     if (randot == 0)
+//     {
+//         x = (x+xa)/2;
+//         y = (y+ya)/2;
+//     }
+
+//     if (randot == 1)
+//     {
+//         x = (x+xb)/2;
+//         y = (y+yb)/2;
+//     }
+
+//     if(randot == 2)
+//     {
+//         x = (x+xc)/2;
+//         y = (y+yc)/2;
+//     }
+//     Console.SetCursorPosition(x,y);
+//     Console.WriteLine('+');
+//     count++;
+// }
+
+string[] array = {"hello", "2", "world", ":-E"};
+int counter = 0;
+string[] result = new string[array.Length];
+
+foreach(string str in array)
 {
-    int randot = new Random().Next(0, 3);
-    if (randot == 0)
+    if(str.Length <= 3)
     {
-        x = (x+xa)/2;
-        y = (y+ya)/2;
+        Console.WriteLine(str);
+        Array.Resize(ref result, result.Length-1);
+        result[counter] = str;
+        counter++;
     }
-
-    if (randot == 1)
-    {
-        x = (x+xb)/2;
-        y = (y+yb)/2;
-    }
-
-    if(randot == 2)
-    {
-        x = (x+xc)/2;
-        y = (y+yc)/2;
-    }
-    Console.SetCursorPosition(x,y);
-    Console.WriteLine('+');
-    count++;
 }
+Console.WriteLine(array[0]);
+Console.WriteLine(result.Length);
+Console.WriteLine("[{0}]", string.Join(", ", result));
