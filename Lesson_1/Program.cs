bcd4542608c3da4eecd8644e19a8cba0fd67fabb
iachichurov <a.iachichurov@gmail.com>
2022-09-17 21:09:50 +1000

    some play played

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/ne.w-console-template for more information
-Console.WriteLine("Дароу, ты хто?!");
+Console.Write("Дароу, ты хто?!");
 var username = Console.ReadLine();
-Console.WriteLine("Привет, ");
-Console.WriteLine(username);
+Console.Write("Привет, ");
+Console.Write(username);
