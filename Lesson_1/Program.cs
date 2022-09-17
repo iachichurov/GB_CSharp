7f9056590b36f724667fb0aa07c2d03d0ba092dd
iachichurov <a.iachichurov@gmail.com>
2022-09-18 04:14:15 +1000

    some fixes fixed

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -86,16 +86,15 @@
 //     count++;
 // }
 
-string[] array = {"hello", "2", "world", ":-E"};
+string[] array = {"hello", "2", "world", ":-E", "df", "da"};
 int counter = 0;
-string[] result = new string[array.Length];
+string[] result = new string[0];
 
 foreach(string str in array)
 {
     if(str.Length <= 3)
     {
-        Console.WriteLine(str);
-        Array.Resize(ref result, result.Length-1);
+        Array.Resize(ref result, result.Length+1);
         result[counter] = str;
         counter++;
     }
